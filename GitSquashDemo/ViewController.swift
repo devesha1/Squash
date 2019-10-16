13ee0f9f1cd787c653cb9c364292b6e6e0271e7d
devesha1 <deveshideal2005@gmail.com>
2019-10-16 23:11:00 +0530

    Initial Commit

---
+++ GitSquashDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitSquashDemo
+//
+//  Created by Devesh Sharma on 16/10/19.
+//  Copyright © 2019 Devesh Sharma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
